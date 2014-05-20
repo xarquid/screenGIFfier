3655e2bda18fa31f22d4b7888f2b77055e88d436
Alex Manzella <manzopower@icloud.com>
2014-05-20 18:31:47 +0200

    it do the job

---
+++ README.md
@@ -0,0 +1,7 @@
+#screenGIFfier
+
+create a gif from the screen of your connected iPhone (iDevice)
+
+press control+C to stop and create the gif...
+
+this little utility use the Library libMobileDeviceAccess, an open source objC wrapper for the MobileDevice framework
---
+++ screenGIFfier/main.m
@@ -0,0 +1,110 @@
+//
+//  main.m
+//  ScreenGIFfier
+//
+//  Created by Alex Manzella on 20/05/14.
+//
+//
+
+
+#import <Foundation/Foundation.h>
+#import "MobileDeviceAccess.h"
+#import <AppKit/AppKit.h>
+#import <Cocoa/Cocoa.h>
+
+#include <signal.h>
+
+static bool keepRunning = true;
+
+void intHandler() {
+    keepRunning = false;
+}
+
+
+#define TIME_TO_WAIT 0.1
+
+@interface MPListener : NSObject<MobileDeviceAccessListener>
+
+@end
+
+void exportAnimatedGifFromImages(NSArray *images);
+
+int main(int argc, const char * argv[])
+{
+    
+    @autoreleasepool {
+        
+        signal(SIGINT, intHandler);
+        
+        
+        MPListener *listener=[[[MPListener alloc] init] autorelease];
+        [[MobileDeviceAccess singleton] setListener:listener];
+        [[MobileDeviceAccess singleton] waitForConnection];
+        
+        for(AMDevice *device in [[MobileDeviceAccess singleton] devices]){
+            AMScreenshotService* screenshotService=[device newAMScreenshotService];
+            NSMutableArray *arr=[[[NSMutableArray alloc] init] autorelease];
+            while (keepRunning) {
+                NSLog(@"image captured %lu\n PRESS Control+C to stop and create the GIF",arr.count+1);
+                NSImage *screen=[screenshotService getScreenshot];
+                if(screen)
+                    [arr addObject:screen];
+                sleep(TIME_TO_WAIT);
+            }
+            exportAnimatedGifFromImages(arr);
+        }
+        //
+        //    dispatch_semaphore_wait(dispatch_semaphore_create(0), DISPATCH_TIME_FOREVER);
+        
+    }
+    return 0;
+}
+
+#import <ImageIO/ImageIO.h>
+
+void exportAnimatedGifFromImages(NSArray *images)
+{
+    if (!images) {
+        return;
+    }
+    NSString *path = [NSString stringWithFormat:[@"~/Desktop/screen%@.gif" stringByExpandingTildeInPath],[NSDate date]];
+    
+    CGImageDestinationRef destination = CGImageDestinationCreateWithURL((CFURLRef)[NSURL fileURLWithPath:path],
+                                                                        kUTTypeGIF,
+                                                                        images.count,
+                                                                        NULL);
+    
+    NSDictionary *frameProperties = [NSDictionary dictionaryWithObject:[NSDictionary dictionaryWithObject:[NSNumber numberWithFloat:TIME_TO_WAIT] forKey:(NSString *)kCGImagePropertyGIFDelayTime]
+                                                                forKey:(NSString *)kCGImagePropertyGIFDictionary];
+    NSDictionary *gifProperties = [NSDictionary dictionaryWithObject:[NSDictionary dictionaryWithObject:[NSNumber numberWithInt:0] forKey:(NSString *)kCGImagePropertyGIFLoopCount]
+                                                              forKey:(NSString *)kCGImagePropertyGIFDictionary];
+    
+    for (NSImage *image in images) {
+        CGImageSourceRef source;
+        source = CGImageSourceCreateWithData((CFDataRef)[image TIFFRepresentation], NULL);
+        CGImageRef maskRef =  CGImageSourceCreateImageAtIndex(source, 0, NULL);
+        CGImageDestinationAddImage(destination, maskRef, (CFDictionaryRef)frameProperties);
+    }
+    
+    
+    CGImageDestinationSetProperties(destination, (CFDictionaryRef)gifProperties);
+    CGImageDestinationFinalize(destination);
+    CFRelease(destination);
+    NSLog(@"Animated GIF file created at %@", path);
+}
+
+
+@implementation MPListener
+
+
+
+- (void)deviceConnected:(AMDevice*)device{
+    NSLog(@"%@",[[MobileDeviceAccess singleton] devices]);
+    
+}
+- (void)deviceDisconnected:(AMDevice*)device{
+    NSLog(@"%@",[[MobileDeviceAccess singleton] devices]);
+    
+}
+
+@end
